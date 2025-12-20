7c3e404c81c087d1caf03e45389342a18b264431
Dani Sarfati <dani@funkervogt.com>
2025-12-20 18:19:29 -0500

    is it working

--- rtl/dataController_top.sv
+++ rtl/dataController_top.sv
@@ -1,25 +1,32 @@
 module dataController_top(
-	input clk32,
+	// clocks:
+	input clk32,					// 32.5 MHz pixel clock
 	input clk8_en_p,
 	input clk8_en_n,
 	input E_rising,
 	input E_falling,
 	
-	input machineType,
+	// system control:
+	input machineType, // 0 - Mac Plus, 1 - Mac LC
+	input maclc_mode,  // 0 = Mac Plus mode, 1 = Mac LC mode
+
 	input _systemReset,
 
+	// 68000 CPU control:
 	output _cpuReset,
 	output [2:0] _cpuIPL,
 
+	// 68000 CPU memory interface:
 	input [15:0] cpuDataIn,
-	input [3:0] cpuAddrRegHi,
-	input [2:0] cpuAddrRegMid,
-	input [1:0] cpuAddrRegLo,
+	input [3:0] cpuAddrRegHi, // A12-A9
+	input [2:0] cpuAddrRegMid, // A6-A4
+	input [1:0] cpuAddrRegLo, // A2-A1
 	input _cpuUDS,
 	input _cpuLDS,	
 	input _cpuRW,
 	output [15:0] cpuDataOut,
 	
+	// peripherals:
 	input selectSCSI,
 	input selectSCC,
 	input selectIWM,
@@ -27,48 +34,55 @@ module dataController_top(
 	input selectSEOverlay,
 	input _cpuVMA,
 	
+	// V8 Video peripheral selects
 	input selectAriel,
-	input [7:0] ariel_data_in,
+	input selectVRAM,
 
+	// V8 Video data buses
+	input [7:0] ariel_data_in,
+	input [31:0] vram_data_in,
+
+	// RAM/ROM:
 	input videoBusControl,	
 	input cpuBusControl,	
 	input [15:0] memoryDataIn,
 	output [15:0] memoryDataOut,
 	input memoryLatch,
 	
+	// keyboard:
 	input [10:0] ps2_key,
 	output capslock, 
+	 
+	// mouse:
 	input [24:0] ps2_mouse,
 	
+	// serial:
 	input serialIn, 
 	output serialOut,	
 	input serialCTS,
 	output serialRTS,
 
+	// RTC
 	input [32:0] timestamp,
 
+	// video:
 	output pixelOut,	
 	input _hblank,
 	input _vblank,
 	input loadPixels,
 	output vid_alt,
 
-	output [10:0] audioOut,
+	// audio
+	output [10:0] audioOut,  // 8 bit audio + 3 bit volume
 	output snd_alt,
 	input loadSound,
 	
+	// misc
 	output memoryOverlayOn,
 	input [1:0] insertDisk,
 	input [1:0] diskSides,
 	output [1:0] diskEject,
-	output [1:0] diskMotor,
-	output [1:0] diskAct,
-
-	output [21:0] dskReadAddrInt,
-	input dskReadAckInt,
-	output [21:0] dskReadAddrExt,
-	input dskReadAckExt,
-
+	// connections to io controller
 	input   [SCSI_DEVS-1:0] img_mounted,
 	input            [31:0] img_size,
 	output           [31:0] io_lba[SCSI_DEVS],
@@ -82,8 +96,7 @@ module dataController_top(
 );
 	
 	parameter SCSI_DEVS = 2;
-	output           [31:0] io_lba[SCSI_DEVS],
-	output  [SCSI_DEVS-1:0] io_rd,
+	
 	output  [SCSI_DEVS-1:0] io_wr,
 	input   [SCSI_DEVS-1:0] io_ack,
 	input             [7:0] sd_buff_addr,
@@ -158,17 +171,17 @@ module dataController_top(
 	reg [15:0] cpu_data;
 	always @(posedge clk32) if (cpuBusControl && memoryLatch) cpu_data <= memoryDataIn;
 
-reg [15:0] cpu_data;
-always @(posedge clk32) if (cpuBusControl && memoryLatch) cpu_data <= memoryDataIn;
+	reg [15:0] cpu_data;
+	always @(posedge clk32) if (cpuBusControl && memoryLatch) cpu_data <= memoryDataIn;
 
-assign cpuDataOut = selectIWM ? iwmDataOut :
-                    selectVIA ? viaDataOut :
-                    selectSCC ? { sccDataOut, 8'hEF } :
-                    selectSCSI ? { scsiDataOut, 8'hEF } :
-                    selectAriel ? {ariel_data_in, ariel_data_in} :  
-                    (cpuBusControl && memoryLatch) ? memoryDataIn : cpu_data;
-	
-assign memoryDataOut = cpuDataIn;
+	assign cpuDataOut = selectIWM ? iwmDataOut :
+	                    selectVIA ? viaDataOut :
+	                    selectSCC ? { sccDataOut, 8'hEF } :
+	                    selectSCSI ? { scsiDataOut, 8'hEF } :
+	                    selectAriel ? {ariel_data_in, ariel_data_in} :  
+	                    (cpuBusControl && memoryLatch) ? memoryDataIn : cpu_data;
+		
+	assign memoryDataOut = cpuDataIn;
 
 	// SCSI
 	ncr5380 #(SCSI_DEVS) scsi(
